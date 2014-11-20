f5fef103df284e4052ec18c9fbcbcd89b2f50c7a
Ted Lawless <lawlesst@gmail.com>
2014-11-20 17:07:06 -0500

    Add 505t test as well.

--- spec/title_search_spec.rb
+++ spec/title_search_spec.rb
@@ -4,11 +4,16 @@ require 'spec_helper'
 describe "Title Search" do
 
   it "700t included in title search: Gothic classics b4156972" do
-    #Quick sanity check
+    #Quick sanity check - exact title match.
     resp = solr_resp_doc_ids_only(title_search_args('gothic classics'))
     resp.should have_document(did("b4156972"), 5)
     #This value is in 700t for this document.
     resp = solr_resp_doc_ids_only(title_search_args('Northanger abbey'))
+    resp.should have_document(did("b4156972"), 5)
+  end
+
+  it "505t included in title search: Gothic classics b4156972" do
+    resp = solr_resp_doc_ids_only(title_search_args("I've a pain in my head"))
     resp.should have_document(did("b4156972"), 5)
   end
 
