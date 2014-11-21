608e4dc712b92f2bec753600677649cd5c02ed46
Ted Lawless <lawlesst@gmail.com>
2014-11-20 19:38:43 -0500

    Passing 700t and 505t.

--- spec/title_search_spec.rb
+++ spec/title_search_spec.rb
@@ -5,11 +5,12 @@ describe "Title Search" do
 
   it "700t included in title search: Gothic classics b4156972" do
     #Quick sanity check - exact title match.
+    match = did('b4156972')
     resp = solr_resp_doc_ids_only(title_search_args('gothic classics'))
-    resp.should have_document(did("b4156972"), 5)
+    resp.should have_document(match, 5)
     #This value is in 700t for this document.
-    resp = solr_resp_doc_ids_only(title_search_args('Northanger abbey'))
-    resp.should have_document(did("b4156972"), 5)
+    resp = solr_resp_doc_ids_only(title_search_args("i've a pain in my head"))
+    resp.should have_document(match, 5)
   end
 
   it "505t included in title search: Gothic classics b4156972" do
