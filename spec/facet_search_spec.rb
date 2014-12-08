52470a298efab7a1a4d83ea84bda1947f5eba5f2
Ted Lawless <lawlesst@gmail.com>
2014-12-08 12:23:27 -0500

    Move facet to separate file.

---
+++ spec/facet_search_spec.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe "Format facet queries" do
+
+  it "Should return a video for a record with multiple 007s" do
+    resp = solr_resp_doc_ids_only(
+        {'q'=>'gathering moss', 'fq'=>'format:Video'}
+    )
+    resp.should have_document(did("b7302566"), 5)
+  end
+
+end
--- spec/title_search_spec.rb
+++ spec/title_search_spec.rb
@@ -21,11 +21,4 @@ describe "Title Search" do
 end
 
 
-describe "Format facet queries" do
 
-  it "Should return a video for a record with multiple 007s" do
-    resp = solr_resp_doc_ids_only({'q'=>'gathering moss', 'fq'=>'format:Video'})
-    resp.should have_document(did("b7302566"), 5)
-  end
-
-end
