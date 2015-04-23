db4cb8d0833c39a5eb2d263fd71f5804d40532dd
Ted Lawless <lawlesst@gmail.com>
2015-04-23 12:47:16 -0400

    Add exact title match examples.

--- spec/default_search_spec.rb
+++ spec/default_search_spec.rb
@@ -10,6 +10,14 @@ describe "Default search" do
     default_search('b2041590', 'remains of the day', 10)
   end
 
+  it "Should match full title strings without quotes" do
+    default_search('b2151715', 'A Pale View of Hills', 2)
+  end
+
+  it "Should match full title strings with quotes" do
+    default_search('b2151715', '"A Pale View of Hills"', 2)
+  end
+
   it "Should match partial title and TOC text" do
     default_search('b3176352', 'Effects of globalization in india', 5)
   end
