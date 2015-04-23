bf006d21a112f63c8da4e2caf41e270bca09bc0f
Ted Lawless <lawlesst@gmail.com>
2015-04-23 10:25:47 -0400

    Add title search that were failing.

--- spec/default_search_spec.rb
+++ spec/default_search_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 
 describe "Default search" do
 
-  it "Should match call numbers" do
+  it "Should match popular items" do
     default_search('b3340555', 'Pubmed', 3)
+  end
+
+  it "Should match partial titles" do
+    default_search('b2041590', 'remains of the day', 10)
   end
 
 end
