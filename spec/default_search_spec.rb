731c4566bfbb5f0626b117e6e3abc621e1bc0a7c
Ted Lawless <lawlesst@gmail.com>
2015-05-06 09:07:15 -0400

    Add groups for default searches.

--- spec/default_search_spec.rb
+++ spec/default_search_spec.rb
@@ -2,29 +2,33 @@ require 'spec_helper'
 
 describe "Default search" do
 
-  it "Should match popular items" do
-    default_search('b3340555', 'Pubmed', 3)
+  describe "Popular items" do
+    it "Should match popular items" do
+      default_search('b3340555', 'Pubmed', 3)
+    end
   end
 
-  it "Should match partial titles" do
-    default_search('b2041590', 'remains of the day', 10)
-    default_search('b7113006', 'time is a toy', 3)
-  end
+  describe "Titles" do
+    it "Should match partial titles" do
+      default_search('b2041590', 'remains of the day', 10)
+      default_search('b7113006', 'time is a toy', 3)
+    end
 
-  it "Should match full title strings without quotes" do
-    default_search('b2151715', 'A Pale View of Hills', 2)
-  end
+    it "Should match full title strings without quotes" do
+      default_search('b2151715', 'A Pale View of Hills', 2)
+    end
 
-  it "Should match full title strings with quotes" do
-    default_search('b2151715', '"A Pale View of Hills"', 2)
-  end
+    it "Should match full title strings with quotes" do
+      default_search('b2151715', '"A Pale View of Hills"', 2)
+    end
 
-  it "Should match partial title and TOC text" do
-    default_search('b3176352', 'Effects of globalization in india', 10)
-  end
+    it "Should match partial title and TOC text" do
+      default_search('b3176352', 'Effects of globalization in india', 10)
+    end
 
-  it "Should prefer primary titles over additional titles" do
-    default_search('b1864577', 'scientific american', 5)
+    it "Should prefer primary titles over additional titles" do
+      default_search('b1864577', 'scientific american', 5)
+    end
   end
 
   describe "Authors" do
